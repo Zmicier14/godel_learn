e3310c5a4fbe967b498b1eb009fe6f0ad076c0cd
Dzmitry Kruts <ryangiggs1482@gmail.com>
2021-01-15 15:24:18 +0300

    Make function to input number

--- task2_2.py
+++ task2_2.py
@@ -1,4 +1,11 @@
-try:
-    b = float(input(f'Please input the number = '))
-except ValueError:
-    print('Please input right number!')
+def input_number(a):
+    b = None
+    while b == None:        #Как-то по-другому можно сделать выход из этого цикла?
+        try:
+            b = float(input(f'Please input the {a} number = '))
+        except ValueError:
+            print('Please input right number!')
+    return b
+
+d = input_number('1st')
+e = input_number('2nd')
