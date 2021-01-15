5f06b772b95bac5f4bc26774a83c1427f85cdb4d
Dzmitry Kruts <ryangiggs1482@gmail.com>
2021-01-15 15:05:35 +0300

    Ready for work!

--- task2_1.py
+++ task2_1.py
@@ -1,4 +1,10 @@
 try:
     a = int(input('Please, input number: '))
+    if a % 4 == 0:
+        print(f'The number: {a} is a multiple of 4!')
+    elif a % 2 == 0:
+        print(f'{a} is even number!')
+    else:
+        print(f'{a} is odd number!')    
 except ValueError:
     print('This is non-int format!')
