40af41ceefcdaa2d201b6ed4ba27cdc9e7929a80
Dzmitry Kruts <ryangiggs1482@gmail.com>
2021-01-25 14:00:52 +0300

    Realese  version of program

--- task4_1.py
+++ task4_1.py
@@ -1,13 +1,10 @@
-from tkinter.filedialog import askopenfile
-
 def reads_file(file_name):
     """ (file open for reading) -> list of str
 
     Return a list of all words (with newlines removed) from open file
     file_name.
     """
-
-    i = 0
+    
     string = ''
     wordlist = []
     for line in file_name:
@@ -16,11 +13,11 @@ def reads_file(file_name):
 
     return wordlist
 
-file_1 = askopenfile(mode='r', title='Select first file')
+file_1 = open(input('Please input the name of the first file: '), 'r')
 string_file_1 = reads_file(file_1)
 file_1.close()
 
-file_2 = askopenfile(mode='r', title='Select first file')
+file_2 = open(input('Please input the name of the second file: '), 'r')
 string_file_2 = reads_file(file_2)
 file_2.close()
 common_strings = []
